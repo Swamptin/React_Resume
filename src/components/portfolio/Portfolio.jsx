c4056a1d24cf5002b011f309d1f79d1a74a99508
swamptin <swamptin@pm.me>
2024-02-27 21:31:21 +0000

    Minor changes to wording on Projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -51,7 +51,7 @@ const Portfolio = () => {
 
   return (
     <section id="portfolio">
-      <h5>My Recent Work</h5>
+      <h5>A selection of my personal</h5>
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
